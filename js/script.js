394ea2dd29eee8e696f38c25fbb3214ec9a43d94
usjose <usjose@hotmail.com>
2017-01-14 17:59:19 -0500

    Taking out 'Categories'

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function(){buildAndShowHomeHTML(categories);}, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function(){buildAndShowHomeHTML();}, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
